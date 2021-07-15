b5ce206b18166a4498cee4acc1c998e0c5f1568c
rmagur1203 <rmagur1203@naver.com>
2021-07-15 14:12:22 +0900

    update

--- src/renderers/common.js
+++ src/renderers/common.js
@@ -2,14 +2,18 @@ var { ipcRenderer } = require('electron');
 const COMMON = require('../events/common.js');
 
 const openInChrome = function (url) {
-    ipcRenderer.send(COMMON.OPEN_AT_CHROME, url);
+    if (url)
+        ipcRenderer.send(COMMON.OPEN_AT_CHROME, url);
+    else {
+
+    }
 };
 
 ipcRenderer.on(COMMON.OPEN_AT_CHROME, (event, args) => {
-    if (args.code == "OK"){
+    if (args.code == "OK") {
         console.log("Open operation in chrome was successful.");
     }
-    else if (args.code == "ERR"){
+    else if (args.code == "ERR") {
         console.log("An error occurred during the open operation in chrome.");
         console.log(args.err)
     }
